a10ebc885ec284b2d21fdd88ad5d5f53cfd63f65
pratik0007 <pratikgondane07@gmail.com>
2023-05-30 15:08:05 +0530

    Icon flutter added

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,73 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('FirstApp'),
-          centerTitle: true,
-          backgroundColor: Colors.redAccent,
-        ),
-        body: Center(
-          child: Text(
-            'Alpha Version',
-            style: TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2,
-              color: Colors.amberAccent,
-              fontFamily: 'IndieFlower',
-            ),
-          ),
-        ), //center
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Text('Xplore'),
-          backgroundColor: Colors.cyanAccent[600],
-        ),
-      ), //scaffold is used to allow basic layout
+void main() => runApp(const MaterialApp(
+      home: Home(),
     ));
+
+//Below code is used to help hot restart flutter app.'stateless widget'
+/*Two type of Widgets, 1) Stateless Widgets and Stateful Widget
+Below code section is Stateless widget.
+Stateless Widget : the state of the widget cannot change over time
+Stateful Widgets: the state of the widget can change over time */
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('FirstApp v2'),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Center(
+        child: IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.alternate_email),
+            color: Colors.amberAccent),
+        // child: OutlinedButton.icon(
+        //   onPressed: () {
+        //     print('mail clicked');
+        //   },
+        //   icon: const Icon(Icons.mail), label: Text('mail me'), //Simple button
+
+        // onPressed: () {
+        //   print('Added');
+        // },
+        // child: const Text('click me'),
+        // color: Colors.amberAccent,
+      ),
+
+      // child: Icon(
+      //   Icons.airplanemode_inactive_rounded,
+      //   color: Colors.lightGreenAccent,
+      //   size: 50.0,
+      // ),
+      // children: [
+      //   Icon(
+      //     Icons.airport_shuttle,
+      //   ),
+      //   Image(
+      //     /* we can also write as
+      //   Image.assest('asset/windows.jpg'), */
+      //     //for image to be shown for local drive we use Asset image method.
+      //     image: AssetImage('asset/windows.jpg'),
+      //     fit: BoxFit.cover, //use for making full screen image
+      //     height: double.infinity,
+      //     width: double.infinity,
+      //     alignment: Alignment.center,
+      //   ),
+      //   // for image to be shown for internet we use Network image.
+      //   // image: NetworkImage(
+      //   // 'https://www.talkesport.com/wp-content/uploads/valorant-new-map-1-696x392.jpg'))), //center
+      // ],
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.cyanAccent[600],
+        child: const Text('Xplore'),
+      ),
+    ); //scaffold is used to allow basic layout
+  }
+}
